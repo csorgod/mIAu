1303418ac248535c88a6e44667ef605f92fd140c
Guilherme Csorgo Henriques <csorgodev@hotmail.com>
2021-07-03 13:09:38 -0300

    Initial commit. Added all project strucutre besides some notebooks for audio download and treatment

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Um projeto de IA e Machine Learning para detectar sentimentos de felinos através dos sons emitidos por eles.',
+    author='Guilherme Csorgo',
+    license='',
+)
